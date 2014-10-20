29923cd15048b3de089fae20ab01957c2fc0fb38
Yoshie <yoshiemuranaka@gmail.com>
2014-10-20 16:02:18 -0400

    moreInfo on click working

--- public/script.js
+++ public/script.js
@@ -17,20 +17,35 @@ var g = svg.selectAll('g')
 	.enter().append('g')
 
 g.append('circle')
+	.attr('r', 0)
 .transition().duration(2000).ease('elastic')
 		.attr('cx', function(d, i) { return (i+1) * 50 })
 		.attr('cy', 50)
 		.attr('r', 10)
-		.style('fill', 'orange')
+		.style('fill', 'brown')
 
-g.append('text')
-		.attr('x', function(d, i) { return (i+1) * 50 })
-		.attr('y', 50)
-		.attr('text-anchor', 'middle')
-		.text(function(d) { return d.author })
+// g.append('text')
+// 	// .attr('class', 'hidden')
+// 	.attr('x', function(d, i) { return (i+1) * 50 })
+// 	.attr('y', 50)
+// 	.attr('text-anchor', 'middle')
+// 	.text(function(d) { return d.author })
+
+var circles = d3.selectAll('circle')
+	.on('click', showInfo)
 
 
+function showInfo(){
 
+thisG = d3.select(this)
+// console.log(thisG.data()[0].author)
+
+var div = d3.select('.showInfo')
+	.style("left", 100)
+  .style("top", 100)
+	.text(thisG.data()[0].author)
+
+}
 
 	// svg.selectAll('text')
 	// 	.data(reasons)
