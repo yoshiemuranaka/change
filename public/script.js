87d0bfc4cd601d625c0eea2d497d5a0bd8d5e74e
Yoshie <yoshiemuranaka@gmail.com>
2014-10-25 19:05:05 -0400

    added selected class style

--- public/script.js
+++ public/script.js
@@ -5,42 +5,53 @@ var color = ["#156b87", "#876315", "#543510", "#872815"]
 d3.json('http://localhost:4567/reasons', function(data){  
 
 
-var svg = d3.select("body").append("svg")
-  .attr("width", size)
-  .attr("height", size)
+	var svg = d3.select("body").append("svg")
+	  .attr("width", size)
+	  .attr("height", size)
 
 
-var pack = d3.layout.pack()
-    .size([size, size])
-    .sort(d3.descending)
-    .padding(3)
+	var pack = d3.layout.pack()
+	    .size([size, size])
+	    .sort(d3.descending)
+	    .padding(3)
 
 
-svg.selectAll("circle")
-    .data(pack.nodes(data).slice(1))
-  .enter().append("circle")
-  	.attr('r', 0)
-		.transition().duration(2000).ease('elastic')
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
-    .attr("r", function(d) { return d.r; })
-    .style('fill', function(d) {return color[d.time_of_day]});
+	svg.selectAll("circle")
+	    .data(pack.nodes(data).slice(1))
+	  .enter().append("circle")
+	  	.attr('r', 0)
+			.transition().duration(2000).ease('elastic')
+	    .attr("cx", function(d) { return d.x; })
+	    .attr("cy", function(d) { return d.y; })
+	    .attr("r", function(d) { return d.r; })
+	    .style('fill', function(d) {return color[d.time_of_day]});
 
- d3.selectAll('circle').on('click', showInfo)
- 	 	.on('mouseover', hoverTrue)
- 	 	.on('mouseout', hoverFalse)
- } )
+	 d3.selectAll('circle').on('click', showInfo)
+	 	 	.on('mouseover', hoverTrue)
+	 	 	.on('mouseout', hoverFalse)
 
-function showInfo(d){
-	var div = d3.select('.content')
-	.text(d.content)
+	 d3.select('button').on('click', filter)
+ 
+
+} )//end AJAX
+
+function filter(){
+	var svg = d3.select('svg')
+	console.log(svg)
 }
 
-function hoverTrue(d){
+function showInfo(d){
+	d3.selectAll('circle').classed('selected', false)
+	var div = d3.select('.content')
+	.text(d.content)
+	d3.select(this).classed('selected', true)
+}
+
+function hoverTrue(){
 	d3.select(this).classed('text-hover', true)
 }
 
-function hoverFalse(d){
+function hoverFalse(){
 	d3.select(this).classed('text-hover', false)
 }
 
